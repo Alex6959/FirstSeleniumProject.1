5f8ac0b3f9802915b568117a3ea3ff0f5bbabe9c
Alex6959 <stepanenko6288@yandex.com>
2025-03-23 14:33:26 +0200

    Add1

--- src/main/java/HomeWork/LoginTestHome.java
+++ src/main/java/HomeWork/LoginTestHome.java
@@ -1,10 +1,16 @@
 package HomeWork;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTestHome extends TestBaseHome{
    @Test
     public void loginPositiveTestHome(){
-
+       click();
+       type(By.name("email"), "stepanenko6288@yandex.com");
+       type(By.name("password"), "6969Sasha#%");
+       click(By.name("Registration"));
+       Assert.assertTrue(isAlertDisplayed());
    }
 }
